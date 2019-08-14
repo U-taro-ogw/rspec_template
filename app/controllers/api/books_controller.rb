030691e8388ef2076fdb6a02180d9161d61bf161
U-taro-ogw <51327986+U-taro-ogw@users.noreply.github.com>
2019-08-14 16:20:14 +0900

    update books_table
    - add null false
    - refactor

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -1,6 +1,6 @@
 class Api::BooksController < ApplicationController
   def index
-    books = Book.all.map(&:attributes).map(&:to_json)
+    books = Book.all.map{ |book| book.attributes.to_json }
     render json: { status: 200, books: books }
   end
 end
