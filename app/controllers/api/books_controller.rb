be98c48a8cdb62ba11780ff71b277d4d60a025f5
U-taro-ogw <51327986+U-taro-ogw@users.noreply.github.com>
2019-08-14 12:31:40 +0900

    create get index test

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -1,5 +1,6 @@
 class Api::BooksController < ApplicationController
   def index
-    render json: { hoge: 'fuga' }
+    books = Book.all.map(&:attributes).map(&:to_json)
+    render json: { status: 200, books: books }
   end
 end
--- spec/requests/api/books_spec.rb
+++ spec/requests/api/books_spec.rb
@@ -3,9 +3,38 @@ require 'rails_helper'
 RSpec.describe 'Books', type: :request do
   describe 'GET api/books' do
     subject { proc { get api_books_path } }
-    it 'hoge' do
-      subject.call
-      expect(json_response[:hoge]).to eq 'fuga'
+
+    shared_examples '一覧検索成功' do
+      it '検索成功のステータスを返却する' do
+        subject.call
+        expect(json_response[:status]).to eq 200
+      end
+
+      it 'bookを全件返却する' do
+        subject.call
+        expect(json_response[:books].length).to eq books_count
+      end
+    end
+
+    context 'bookが1件以上存在する場合' do
+      context 'bookが1件の場合' do
+        let!(:books) { create(:book) }
+        let(:books_count) { 1 }
+
+        it_behaves_like '一覧検索成功'
+      end
+
+      context 'bookがn件の場合' do
+        let!(:books) { (1..2).map{ |i| create(:book) } }
+        let(:books_count) { books.length }
+
+        it_behaves_like '一覧検索成功'
+      end
+    end
+
+    context 'bookが0件の場合' do
+      let(:books_count) { 0 }
+      it_behaves_like '一覧検索成功'
     end
   end
 end
