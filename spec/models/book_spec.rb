5307bd47473c762a61d760e63f15f5d4d2f74177
U-taro-ogw <yuutarou0406@gmail.com>
2019-08-14 16:35:30 +0900

    create book model spec

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'bookを登録する' do
+    expect{ create(:book) }.to change{ Book.count }.by(1)
+  end
+
+  describe 'バリデーション' do
+    describe 'title' do
+      context 'nullの場合' do
+        let!(:book) { build(:book, title: nil) }
+        it 'バリデーションエラーを発生させる' do
+          expect(book).not_to be_valid
+        end
+
+        it 'bookを登録しない' do
+          expect{ book.save }.to change{ Book.count }.by(0)
+        end
+      end
+    end
+
+    describe 'author' do
+      context 'nullの場合' do
+        let!(:book) { build(:book, author: nil) }
+        it 'バリデーションエラーを発生させる' do
+          expect(book).not_to be_valid
+        end
+
+        it 'bookを登録しない' do
+          expect{ book.save }.to change{ Book.count }.by(0)
+        end
+      end
+    end
+
+    describe 'price' do
+      context 'nullの場合' do
+        let!(:book) { build(:book, price: nil) }
+        it 'バリデーションエラーを発生させる' do
+          expect(book).not_to be_valid
+        end
+
+        it 'bookを登録しない' do
+          expect{ book.save }.to change{ Book.count }.by(0)
+        end
+      end
+    end
+  end
 end
