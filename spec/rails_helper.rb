e6341a53fc7f98cdcd6d6214fd9f30466c348468
U-taro-ogw <51327986+U-taro-ogw@users.noreply.github.com>
2019-08-28 14:54:08 +0900

    change rails_helper.rb
    - add config database_cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -64,4 +64,16 @@ RSpec.configure do |config|
 
 
   config.include FactoryBot::Syntax::Methods
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
