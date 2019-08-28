505d0e6b50dc385c94509df7a2293963959f05b2
U-taro-ogw <51327986+U-taro-ogw@users.noreply.github.com>
2019-08-28 14:54:51 +0900

    create get show test

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -1,6 +1,23 @@
 class Api::BooksController < ApplicationController
   def index
-    books = Book.all.map{ |book| book.attributes.to_json }
+    books = Book.all.map(&:to_json)
     render json: { status: 200, books: books }
+  end
+
+  def show
+    book = Book.find_by(id: book_id_params)
+    p book
+    unless book
+      render json: { status: 404, message: 'Not Found' }
+    else
+      render json: { status: 200, book: book.to_json }
+    end
+  end
+
+
+  private
+
+  def book_id_params
+    params[:id]
   end
 end
