b3e1821811d371aa0a2786a45277f5731f6e0ed3
U-taro-ogw <yuutarou0406@gmail.com>
2019-09-13 19:29:32 +0900

    fix book_create

--- spec/requests/api/books_spec.rb
+++ spec/requests/api/books_spec.rb
@@ -7,10 +7,10 @@ RSpec.describe 'Books', type: :request do
     shared_examples '一覧検索成功' do
       it 'bookを全件返却する' do
         subject.call
-        response = json_response[:data]
-        expect(response.length).to eq books_count
+        response_data = json_response[:data]
+        expect(response_data.length).to eq books_count
 
-        response.zip(books).each do |response_book, book|
+        response_data.zip(books).each do |response_book, book|
           expect(response_book[:type]).to eq 'book'
           expect(response_book[:id]).to eq book.id.to_s
 
@@ -63,11 +63,11 @@ RSpec.describe 'Books', type: :request do
       it 'bookを返却する' do
         subject.call
 
-        response = json_response[:data]
-        expect(response[:type]).to eq 'book'
-        expect(response[:id]).to eq book_id.to_s
+        response_data = json_response[:data]
+        expect(response_data[:type]).to eq 'book'
+        expect(response_data[:id]).to eq book_id.to_s
 
-        response_attribute = response[:attributes]
+        response_attribute = response_data[:attributes]
         expect(response_attribute[:title]).to eq book.title
         expect(response_attribute[:author]).to eq book.author
         expect(response_attribute[:price]).to eq book.price
@@ -80,6 +80,70 @@ RSpec.describe 'Books', type: :request do
       let(:book_id) { book.id + 1 }
 
       it_behaves_like 'ステータス404を返却する'
+    end
+  end
+
+  describe 'POST api/books' do
+    subject { proc { post api_books_path, params: post_params } }
+    let(:post_params) { { books: books_param } }
+
+    context '登録できるパラメータの場合' do
+      shared_examples '登録成功' do
+        it_behaves_like 'ステータス201を返却する'
+
+        it 'bookをパラメータの件数分作成する' do
+          expect { subject.call }.to change { Book.count }.by(param_count)
+        end
+      end
+
+      context '登録件数が1件の場合' do
+        let(:param_count) { 1 }
+        let(:books_param) { [ { title: 'title', author: 'author', price: 1 } ] }
+
+        it_behaves_like '登録成功'
+
+        it 'パラメータ通りの値で登録する' do
+          subject.call
+          response_data = json_response[:data]
+
+          response_data.each do |response_book|
+            expect(response_book[:type]).to eq 'book'
+            response_attribute = response_book[:attributes]
+            expect(response_attribute[:title]).to eq post_params[:books][0][:title]
+            expect(response_attribute[:author]).to eq post_params[:books][0][:author]
+            expect(response_attribute[:price]).to eq post_params[:books][0][:price]
+          end
+        end
+      end
+
+      context '登録件数が複数件の場合' do
+        let(:param_count) { 2 }
+        let(:books_param) { (1..param_count).map { |i| { title: "title_#{i}", author: "author_#{i}", price: i } } }
+
+        it_behaves_like '登録成功'
+
+        it 'パラメータ通りの値で登録する' do
+          subject.call
+          response_data = json_response[:data]
+
+          response_data.zip(post_params[:books]) do |response_book, post_param|
+            expect(response_book[:type]).to eq 'book'
+            response_attribute = response_book[:attributes]
+            expect(response_attribute[:title]).to eq post_param[:title]
+            expect(response_attribute[:author]).to eq post_param[:author]
+            expect(response_attribute[:price]).to eq post_param[:price]
+          end
+        end
+      end
+    end
+
+    xcontext 'バリデーションエラーが発生する場合' do
+
+      context '必須項目が入力されていない場合' do
+        let(:books_param) { [ { title: '', author: 'author', price: 1 } ] }
+        it_behaves_like 'ステータス400を返却する'
+
+      end
     end
   end
 end
