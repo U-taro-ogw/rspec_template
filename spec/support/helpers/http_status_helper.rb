ef28396900f0f0c5e64b1e9b9cb5c58e51258a63
U-taro-ogw <51327986+U-taro-ogw@users.noreply.github.com>
2019-09-13 19:29:32 +0900

    fix book_create

--- spec/support/helpers/http_status_helper.rb
+++ spec/support/helpers/http_status_helper.rb
@@ -5,6 +5,20 @@ shared_examples 'ステータス200を返却する' do
   end
 end
 
+shared_examples 'ステータス201を返却する' do
+  it 'ステータス201を返却する' do
+    subject.call
+    expect(response).to have_http_status(:created)
+  end
+end
+
+shared_examples 'ステータス400を返却する' do
+  it 'ステータス400を返却する' do
+    subject.call
+    expect(response).to have_http_status(:bad_request)
+  end
+end
+
 shared_examples 'ステータス404を返却する' do
   it 'ステータス404を返却する' do
     subject.call
