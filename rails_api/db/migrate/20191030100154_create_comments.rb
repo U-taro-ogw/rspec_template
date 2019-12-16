f85147866055aa0d1cfdba577ac0c35920353dc5
U-taro-ogw <yuutarou0406@gmail.com>
2019-12-16 17:20:55 +0900

    rubocop対応

--- rails_api/db/migrate/20191030100154_create_comments.rb
+++ rails_api/db/migrate/20191030100154_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-      t.references :book,foreign_key: true, null: false
+      t.references :book, foreign_key: true, null: false
       t.string :text, null: false
 
       t.timestamps
